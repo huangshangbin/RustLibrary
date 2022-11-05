74c34e5b278cd117523c43e04c4633dffdb68da3
huangshangbin <393941336@qq.com>
2022-11-05 10:09:25 +0800

    add StringUtils api

--- RustLibrary/library/src/utils/StringUtils.rs
+++ RustLibrary/library/src/utils/StringUtils.rs
@@ -4,3 +4,46 @@ pub fn splitString(srcStr:&str, splitStr:&str) -> Vec<String> {
     strList
 }
 
+pub fn splitStringGetOneStr(srcStr:&str, splitStr:&str, index:usize) -> String {
+    let strList : Vec<String> = srcStr.split(splitStr).into_iter()
+    .map(move |tempStr| tempStr.to_string()).collect();
+    strList[index].clone()
+}
+
+pub fn replaceString(srcStr : &str, replaceStr : &str, targetStr : &str) -> String {
+    srcStr.replace(replaceStr, targetStr).to_string()
+}
+
+pub fn isExistStringInString(srcStr : &str, existStr : &str) -> bool {
+    srcStr.contains(existStr)
+}
+
+pub fn isUseStringEnd(srcStr : &str, endStr : &str) -> bool {
+    srcStr.ends_with(endStr)
+}
+
+pub fn getStringUsePos(srcStr : &str, startPos : usize, endPos : usize) -> String {
+	srcStr[startPos..endPos + 1].to_string()
+}
+
+pub fn getStringUseCharStart(srcStr : &str, startChar : char) -> String {
+    let mut index : usize = 0;
+    for i in srcStr.chars() {
+        if i == startChar {
+            break;
+        }
+        index = index + 1;
+    }
+    srcStr[index..srcStr.len()].to_string()
+}
+
+pub fn getStringUseCharEnd(srcStr : &str, endChar : char) -> String {
+    let mut index : usize = 0;
+    for i in srcStr.chars() {
+        if i == endChar {
+            break;
+        }
+        index = index + 1;
+    }
+    srcStr[0..index].to_string()
+}
--- RustLibrary/src/main.rs
+++ RustLibrary/src/main.rs
@@ -1,7 +1,12 @@
 use library::utils::StringUtils;
 fn main() {
-    let strList = StringUtils::splitString("huangshangbin", "a");
-    for str in strList {
-        println!("{}", str);
-    }
+    //let srcStr = String::from("huangshangbin");
+    //let subStr = &srcStr[0..6];
+
+    //println!("subStr={}", subStr);
+
+    let srcStr = String::from("huangshangbin");
+    let splitStr = String::from("a");
+    let result = StringUtils::getStringUseCharEnd(srcStr.as_str(), 'a');
+    println!("result={}", result);
 }
