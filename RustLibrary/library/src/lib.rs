3a46fc821e870c58fdce9caac2721cfcfa3968a4
huangshangbin <393941336@qq.com>
2022-11-21 21:45:13 +0800

    屏蔽编译警告

--- RustLibrary/library/src/lib.rs
+++ RustLibrary/library/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
 #[macro_use]
+
 
 pub mod utils;
--- RustLibrary/src/main.rs
+++ RustLibrary/src/main.rs
@@ -1,12 +1,48 @@
-use library::utils::StringUtils;
+#![allow(warnings)]
+trait Animal {
+    fn get_kind(&self) -> String;
+}
+ 
+struct Dog {}
+ 
+impl Animal for Dog {
+    fn get_kind(&self) -> String {
+        "Dog".to_string()
+    }
+}
+ 
+struct Cat {}
+ 
+impl Animal for Cat {
+    fn get_kind(&self) -> String {
+        "Cat".to_string()
+    }
+}
+ 
+// 静态分发
+fn get_animal_static() -> impl Animal {
+    let x = 0;
+    if x == 1 {
+        Dog {}
+    } else {
+        Dog {}
+    }
+}
+ 
+// 动态分发
+fn get_animal_dyn() -> Box<dyn Animal> {
+    let x = 0;
+    if x == 1 {
+        Box::new(Dog {})
+    } else {
+        Box::new(Cat {})
+    }
+}
+ 
 fn main() {
-    //let srcStr = String::from("huangshangbin");
-    //let subStr = &srcStr[0..6];
-
-    //println!("subStr={}", subStr);
-
-    let srcStr = String::from("huangshangbin");
-    let splitStr = String::from("a");
-    let result = StringUtils::getStringUseCharEnd(srcStr.as_str(), 'a');
-    println!("result={}", result);
+    let animal = get_animal_static();
+    println!("{}", animal.get_kind());
+ 
+    let animal = get_animal_dyn();
+    println!("{}", animal.get_kind());
 }
